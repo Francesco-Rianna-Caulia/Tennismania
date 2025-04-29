9cf5a842698f255b7448a824349ed99a591445ad
francesco.rianna <francesco.rianna1998@gmail.com>
2025-04-29 20:44:46 +0200

    create view for login

--- TennismaniaApp/ContentView.swift
+++ TennismaniaApp/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("prova, ciao!")
+            LoginView()
         }
         .padding()
     }
---
+++ TennismaniaApp/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  TennismaniaApp
+//
+//  Created by Francesco Rianna on 29/04/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("andrea pene!")
+            .font(.title)
+            .foregroundStyle(Color.red)
+    }
+}
+
+#Preview {
+    LoginView()
+}

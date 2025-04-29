6fc9760d78c16f1cd3ca54ca8d6d51186c6f1f7d
Andrea Sandomenico <andreasandomenico97@gmail.com>
2025-04-29 20:00:32 +0200

    initial commit

---
+++ TennismaniaApp/TennismaniaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennismaniaAppApp.swift
+//  TennismaniaApp
+//
+//  Created by Ulixe on 29/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TennismaniaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

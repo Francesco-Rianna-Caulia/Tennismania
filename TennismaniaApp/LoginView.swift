e8bfb671bd475639827d33072545cb93506c04b3
francesco.rianna <francesco.rianna1998@gmail.com>
2025-04-30 12:19:01 +0200

    inserted bg img

--- TennismaniaApp/LoginView.swift
+++ TennismaniaApp/LoginView.swift
@@ -9,12 +9,66 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text("andrea pene!")
-            .font(.title)
-            .foregroundStyle(Color.red)
+        ZStack {
+            Image("bg_img")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            // Contenuto principale
+            VStack {
+                Spacer().frame(height: 32)
+                
+                // Logo e titolo
+                Text("Tennis mania")
+                    .font(.system(size: 40, weight: .bold))
+                    .foregroundStyle(Color.green)
+                    .padding(.bottom, 10)
+                
+                Text("Benvenuto nell'app per gli amanti del tennis. Accedi o registrati per continuare.")
+                    .font(.body)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .foregroundStyle(Color.red)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 30)
+                    .padding(.bottom, 50)
+                Spacer()
+                // Pulsanti con spaziatura migliorata
+                VStack(spacing: 20) {
+                    Button(action: {
+                        print("Accedi premuto")
+                    }) {
+                        Text("Accedi")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(15)
+                    }
+                    .padding(.horizontal, 40)
+                    
+                    Button(action: {
+                        print("Registrati premuto")
+                    }) {
+                        Text("Registrati")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .cornerRadius(15)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color.blue, lineWidth: 2)
+                            )
+                    }
+                    .padding(.horizontal, 40)
+                }
+                
+                // Spacer che si espande per spingere il contenuto verso l'alto
+                Spacer()
+            }
+            .padding(.top, 20)
+        }
     }
 }
 
-#Preview {
-    LoginView()
-}
